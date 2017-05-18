7c126185bf31fb3a861358f9ae1d9c5c92c08aaf
Natasha Gitlin <natasha.gitlin8@gmail.com>
2017-05-17 23:18:43 -0400

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -2,19 +2,38 @@ require 'pry'
 
 class String
 
+
+
   def sentence?
-    
+    if self.end_with?(".")
+      true
+    else
+      false
+    end
   end
 
   def question?
-
+    if self.end_with?("?")
+      true
+    else
+      false
+    end
   end
 
   def exclamation?
-
+    if self.end_with?("!")
+      true
+    else
+      false
+    end
   end
 
   def count_sentences
+    # self.split(/[.?!]/).size
 
+    # self.split(/[?!.]\s+[A-Z]/).size
+     self.split(/[.?!]+/).grep(/\S/).count
   end
+
+
 end
